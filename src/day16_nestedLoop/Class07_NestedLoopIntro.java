7fcf6c335ba7db570ec71829c9f370a6a2593c0f
tleafconsultingco <tleaf.consultingco@gmail.com>
2023-03-31 18:37:18 -0500

    week06

--- src/day16_nestedLoop/Class07_NestedLoopIntro.java
+++ src/day16_nestedLoop/Class07_NestedLoopIntro.java
@@ -7,7 +7,7 @@ public class Class07_NestedLoopIntro {
             System.out.println("Wooden Spoon "+i); // repeated 5 times
         }
 
-        // if you need to repeat the loop, you could copy and paste the loop...or make a nested loop
+        //! if you need to repeat the loop, you could copy and paste the loop...or make a nested loop
         for (int i = 0; i < 5; i++) {
             System.out.println("Wooden Spoon "+i); // repeated 5 times
         }
@@ -25,8 +25,8 @@ public class Class07_NestedLoopIntro {
         }
         System.out.println("-----NESTED LOOP");
 
-        for (int i = 0; i < 5; i++) { // outer loop. When it runs 1 time, the inner loop runs. The outer loop will run 5 times.
-            for (int j = 0; j < 5; j++) { // inner loop. Will run 5 times.
+        for (int i = 0; i < 5; i++) { //!! outer loop. When it runs 1 time, the inner loop runs. The outer loop will run 5 times.
+            for (int j = 0; j < 5; j++) { //!! inner loop. Will run 5 times.
                 System.out.println("Wooden Spoon "+j); // results in this being printed 25 times (5 outer x 5 inner).
             }
         }
