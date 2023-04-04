077b453da468b800f7d08cbeb66b2d3d990af20a
tleafconsultingco <tleaf.consultingco@gmail.com>
2023-04-04 13:30:12 -0500

    week08

--- src/day23_arrayList/Practice02_RemoveName.java
+++ src/day23_arrayList/Practice02_RemoveName.java
@@ -1,7 +1,24 @@
 package day23_arrayList;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Practice02_RemoveName {
-}
+    public static void main(String[] args) {
+
+        //!! Create a new ArrayList and add elements
+        ArrayList<String> names = new ArrayList<>(Arrays.asList("John", "Ahmed", "Daniel", "Ahmed", "James","Muhammed"));
+        System.out.println("names = " + names); //names = [John, Ahmed, Daniel, Ahmed, James, Muhammed]
+
+        //!! Use removeAll()
+        names.removeAll(Arrays.asList("Ahmed"));
+        System.out.println("names w/o Ahmed = " + names); //names w/o Ahmed = [John, Daniel, James, Muhammed]
+
+
+
+
+    }//main close
+}//class close
 /*
 Write a program that can remove all the names "Ahmed" from an
 arraylist of String
--- src/day23_arrayList/Practice03_DuplicateElements.java
+++ src/day23_arrayList/Practice03_DuplicateElements.java
@@ -1,7 +1,28 @@
 package day23_arrayList;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Practice03_DuplicateElements {
-}
+    public static void main(String[] args) {
+        
+        //!! Create new ArrayList with elements
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+        System.out.println("list = " + list); //list = [1, 2, 3, 4, 5]
+
+        //!! Create new ArrayList to include duplicates
+        ArrayList<Integer> dupList = new ArrayList<>(list);
+        System.out.println("dupList = " + dupList); //dupList = [1, 2, 3, 4, 5]
+
+        //!! Use addAll() to duplicate list and add to dupList
+        dupList.addAll(list);
+        System.out.println("dupList = " + dupList); //dupList = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+      
+
+        
+        
+    }//main close
+}//class close
 /*
 Write a program that can duplicate the elements of an arraylist
                          ex:
