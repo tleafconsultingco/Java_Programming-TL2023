d1119c325a2331a83ce32311eb1d346f5afe87fe
tleafconsultingco <tleaf.consultingco@gmail.com>
2023-04-25 12:12:32 -0500

    study_0425t

---
+++ src/studyGroup/constructor_0425t/Q04_Test.java
@@ -0,0 +1,20 @@
+package studyGroup.constructor_0425t;
+
+public class Q04_Test {
+
+    //?? Compilation fails at line 1
+
+    /*
+    public Constructor(){ //line 1
+        //&& Constructor name must match the Class name
+        System.out.println("Default");
+    }//constructor
+    */
+
+    public static void main(String[] args){
+        Q04_Test obj = new Q04_Test(); // line 2
+
+    }//main
+
+}//class
+
