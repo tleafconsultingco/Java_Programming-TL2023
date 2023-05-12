355914e2d745f46dac010a2d688636a40a642bc3
tleafconsultingco <tleaf.consultingco@gmail.com>
2023-05-12 17:27:42 -0500

    week11

---
+++ src/day31_inheritance/Class09_ProtectedAccessModifier.java
@@ -0,0 +1,14 @@
+package day31_inheritance;
+
+public class Class09_ProtectedAccessModifier {
+
+    protected static int a = 100;
+
+    protected static void methodA(){
+
+    }//mehthodA
+
+    //? Test in Day31_Class09_Test3
+
+
+}//class
